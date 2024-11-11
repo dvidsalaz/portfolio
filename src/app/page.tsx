74ed5bde5623e076b12d658205e967d1800e4fa7
David S <dvidsalaz@gmail.com>
2024-11-10 22:40:14 -0600

    section development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,87 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className=" min-h-screen">
+      {/* Main Content */}
+      <section
+        id="projects"
+        className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden"
+      >
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute inset-0 w-full h-full object-cover -z-10"
+        >
+          <source src="/videos/watch.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className=" text-sm sm:text-base lg:text-lg relative z-10 flex items-center justify-center flex-col">
+          <h1 className=" font-light text-white">Finance Student</h1>
+          <h1 className=" text-6xl font-light">DAVID A. SALAZAR</h1>
+          <h1 className=" font-light">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor.
+          </h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </section>
+      <section className=" flex flex-col gap-8 p-6 sm:p-10 lg:p-20 bg-white min-h-screen">
+        <div className="flex items-center justify-center h-screen">
+          <h1 className="text-sm sm:text-base lg:text-lg text-black">
+            Welcome to Next.js
+          </h1>
+        </div>
+      </section>
+      <section className=" flex flex-col gap-8 p-6 sm:p-10 lg:p-20 bg-black min-h-screen ">
+        <div className="relative flex min-h-screen flex-col ">
+          <header>
+            <div className="flex-col items-center justify-center h-screen">
+              <div className="flex items-center border-2 border-red-500 justify-center padding">
+                <span className=" border-2 border-red-500">
+                  <Image
+                    src="/images/DSLogo.svg"
+                    alt="logo"
+                    width="36"
+                    height="36"
+                  />{" "}
+                </span>
+                <h1 className="text-4xl border-2 border-purple-500 font-light text-white">
+                  DAVID A. SALAZAR
+                </h1>
+              </div>
+              <h1 className="text-3xl font-">Welcome to Next.js</h1>
+            </div>
+          </header>
+        </div>
+      </section>
+      <footer className="flex flex-col gap-8 bg-bronzeOrange p-6 sm:p-10 lg:p-20 items-center text-center">
+        <div className=" flex flex-col lg:flex-row items-center gap-8 justify-between w-full ">
+          <nav className="text-white flex gap-4 items-center text-sm sm:text-base lg:text-lg">
+            <Link className=" hover:text-gray-300 hover:underline" href="/team">
+              Our Team
+            </Link>
+            <Link
+              className=" hover:text-gray-300 hover:underline"
+              href="/services"
+            >
+              Our Services
+            </Link>
+            <Link
+              className=" hover:text-gray-300 hover:underline"
+              href="/portfolio"
+            >
+              Our Portfolio
+            </Link>
+          </nav>
+          <div>
+            <p className="text-white text-sm sm:text-base lg:text-lg text-center">
+              Copyright &copy; 2024 LP Exteriors LLC. All rights reserved.
+            </p>
+          </div>
+        </div>
       </footer>
-    </div>
+    </main>
   );
 }
